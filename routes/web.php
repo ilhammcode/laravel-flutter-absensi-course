fb16daa9eac5ddc03a7ed7f15e6287c75bedb8b7
ilhammcode <ilhamcode.it@gmail.com>
2024-03-29 13:23:17 +0800

    Penambahan Template Engine

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BackendTemplateController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//backend
+Route::get('/backend_template', [BackendTemplateController::class, 'index'])->name('backend_template');
